6c8124efcce689678d821188c759461d7b85fbd5
Scott Jackson <scottjacksonx@gmail.com>
2010-12-31 13:40:03 +1000

    Added support for HD videos with \&hd=1

--- ntyoutube.py
+++ ntyoutube.py
@@ -9,7 +9,10 @@ app = web.application(urls, globals())
 class ntyoutube:
 	def GET(self):
 		url = "http://youtube.com/watch?v=" + web.input().v
-		web.redirect(YouTubeURLMaker.getYouTubeDownloadLink(url, False))
+		hd = False
+		if web.input().hd == "1":
+			hd = True
+		web.redirect(YouTubeURLMaker.getYouTubeDownloadLink(url, hd))
 
 if __name__ == "__main__":
 	app.run()
